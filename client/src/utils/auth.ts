4fec41075206b478ee4987b188c6abb5dd484329
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-01 18:21:03 -0500

    update

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,5 +1,14 @@
 // use this to decode a token and get the user's information out of it
-import { jwtDecode } from 'jwt-decode';
+// import { jwtDecode } from 'jwt-decode';
+import { type JwtPayload, jwtDecode } from 'jwt-decode';
+
+interface ExtendedJwt extends JwtPayload {
+  data:{
+    username:string,
+    email:string,
+    id:string
+  }
+};
 
 interface UserToken {
   name: string;
@@ -10,7 +19,8 @@ interface UserToken {
 class AuthService {
   // get user data
   getProfile() {
-    return jwtDecode(this.getToken() || '');
+    // return jwtDecode(this.getToken() || '');
+    return jwtDecode<ExtendedJwt>(this.getToken() || '');
   }
 
   // check if user's logged in
@@ -24,11 +34,11 @@ class AuthService {
   isTokenExpired(token: string) {
     try {
       const decoded = jwtDecode<UserToken>(token);
-      if (decoded.exp < Date.now() / 1000) {
+      // if (decoded.exp < Date.now() / 1000) {
+        if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
         return true;
-      } 
-      
-      return false;
+      }             
+      // return false;
     } catch (err) {
       return false;
     }
@@ -36,7 +46,9 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    // return localStorage.getItem('id_token');
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;    
   }
 
   login(idToken: string) {
