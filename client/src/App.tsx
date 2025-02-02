30d08656a832c2a259e9688f710d00f4499df73d
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-01 20:21:22 -0500

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,57 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+// import './App.css';
+// import { Outlet } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   );
+// }
+
+import "./App.css";
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar"; 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+
+// Create an HTTP link to connect to Apollo Server
+const httpLink = createHttpLink({
+  uri: "/graphql", // Adjust if backend runs on a different port
+});
+
+// Set up authentication middleware to include token in headers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Create Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
