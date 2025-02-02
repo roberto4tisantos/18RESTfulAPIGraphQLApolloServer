7b70659dbe3373f992a6ecef19aebedbcaa555ad
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-01 19:11:43 -0500

    update

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,14 +1,59 @@
+// const typeDefs = `
+//   type Class {
+//     _id: ID!
+//     name: String
+//     building: String
+//     creditHours: Int
+//   }
+
+//   type Query {
+//     classes: [Class]
+//   }
+// `;
+
 const typeDefs = `
-  type Class {
-    _id: ID!
-    name: String
-    building: String
-    creditHours: Int
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+    bookCount: number
   }
 
-  type Query {
-    classes: [Class]
+  type Book {
+    _id: ID
+  title: String
+  authors: []!
+  description: String
+  image: String
+  link: String
+  } 
+  
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
   }
-`;
+  
+  type Auth {
+    token: ID!
+    user: User
+  }  
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    book: [Book]!
+    book(bookId: ID!): Book
+    me: User
+  }  
+
+  type Mutation {
+    addUser(input: UserInput!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(input: BookInput!): Book
+  }
+`;  
 
 export default typeDefs;
