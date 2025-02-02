4cc849a126d4d31ebbaec38ea1397bbd6f10bbc7
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-02 10:54:57 -0500

    update

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,22 +1,47 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-// Update the URI with your database name
-const connectionString = 'mongodb://127.0.0.1:27017/googlebooks'; 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect(connectionString);
+const db = async (): Promise<typeof mongoose.connection> => {
+    try {
+      await mongoose.connect('mongodb://127.0.0.1:27017/googlebooks');
+      console.log('MongoDB connected successfully!');
 
-mongoose.connection.on('connected', () => {
-    console.log('MongoDB connected successfully!');
-});
+      mongoose.connection.on('connected', () => {
+      console.log('MongoDB connected successfully!');
+      });
+      
+      mongoose.connection.on('error', (err) => {
+      console.log('MongoDB connection error:', err);
+      });      
+      return mongoose.connection;
+
+    } catch (error) {
+      console.error('MongoDB connection error:', error);
+      throw new Error('MongoDB connection failed.');
+    }
+  };
+
+
+// Update the URI with your database name
+// const connectionString = 'mongodb://127.0.0.1:27017/googlebooks'; 
+// Wrap Mongoose around local connection to MongoDB
+// mongoose.connect(connectionString);
+
+// mongoose.connection.on('connected', () => {
+//     console.log('MongoDB connected successfully!');
+// });
   
-mongoose.connection.on('error', (err) => {
-    console.log('MongoDB connection error:', err);
-});
+// mongoose.connection.on('error', (err) => {
+//     console.log('MongoDB connection error:', err);
+// });
 
 // Export the URI so it can be used in your main server file
 // export default connectionString; 
 
 // Export connection 
-export default mongoose.connection;
+// export default mongoose.connection;
+
+export default db;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'node:path';
+import type { Request, Response } from 'express';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
@@ -9,6 +10,7 @@ import { expressMiddleware } from '@apollo/server/express4';
 
 // Import the two parts of a GraphQL schema
 import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from '../src/utils/auth.js';
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,7 +21,7 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
 
   await server.start();
-  // await db.connect();
+  await db();
 
   const app = express();
   const PORT = process.env.PORT || 3001;
@@ -27,7 +29,12 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
+// app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));  
 
 // if we're in production, serve client/build as static assets
 // if (process.env.NODE_ENV === 'production') {
@@ -36,19 +43,26 @@ const startApolloServer = async () => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/dist')));
 
-  app.get('*', (_req, res) => {
+  // app.get('*', (_req, res) => {
+    app.get('*', (_req: Request, res: Response) => {  
     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
   });
 };
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);    
-    });
-  });
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`🌍 Now listening on localhost:${PORT}`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);    
+//     });
+//   });
+
+app.listen(PORT, () => {
+  console.log(`API server running 🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);    
+});
+
 };  
 
 // Call the async function to start the server
